24a8b66907dbb7f596db89d6c3284acd13608409
Guillem <guillem.pons@badiapp.com>
2018-10-18 16:30:55 +0200

    Add Spanish Tagger as an extra class

---
+++ Spaniard.py
@@ -0,0 +1,6 @@
+class SpanishTagger(object):
+
+    """ class to do POS-part of speech- tagger in Spanish.
+    The method is fully based upon N-gram """
+
+    def __init__(self):
--- subject_extraction.py
+++ subject_extraction.py
@@ -90,14 +90,6 @@ class _Description(object):
 
 
 
-class SpanishTagger(object):
-
-    """ class to do POS-part of speech- tagger in Spanish.
-    The method is fully based upon N-gram """
-
-    def __init__(self):
-
-
 
 
 
